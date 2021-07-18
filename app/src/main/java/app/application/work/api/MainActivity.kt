095ae851b2221c335309a0a5191432570cba2ca4
ofael <fael_assis@hotmail.com>
2021-07-18 14:32:52 -0300

    working with api

---
+++ app/src/main/java/app/application/work/api/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.application.work.api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
